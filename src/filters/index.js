5322504abe8a6654870e8971b7b46db3d77a31b1
fuqiang <823342479@qq.com>
2017-09-12 01:19:56 +0800

    原文件

---
+++ src/filters/index.js
@@ -0,0 +1,6 @@
+import {normalTime} from './timeFormat';
+// module.exports={
+//     normalTime
+// };
+//webpack 2.0 在webpack打包的时候，可以在js文件中混用require和export。但是不能混用import 以及module.exports。
+export default normalTime;
